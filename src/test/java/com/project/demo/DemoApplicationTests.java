89b7c54529f92f98e7d8edaa5ab9286c71c2234d
jalvarezme <jalvarezme@ucenfotec.ac.cr>
2025-08-11 13:40:37 -0600

    fixing ci problems

--- src/test/java/com/project/demo/DemoApplicationTests.java
+++ src/test/java/com/project/demo/DemoApplicationTests.java
@@ -2,9 +2,17 @@ package com.project.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.project.demo.logic.entity.auth.GoogleAuthService;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DemoApplicationTests {
+
+	@MockBean
+	private GoogleAuthService googleAuthService;
 
 	@Test
 	void contextLoads() {
